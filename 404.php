7f0a5478a435c3950b64b9f9e59a6f74e6d1e0df
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2016-02-11 10:23:53 +0200

    test2

--- 404.php
+++ 404.php
@@ -5,6 +5,9 @@
         <p>Sorry, this page does not exist or has been removed.</p>
 	    hi!
 	    <h2>what?</h2>
+	    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloremque dolorum, earum eos eum eveniet expedita illum incidunt ipsam ipsum labore maiores neque numquam porro saepe sit velit veniam voluptas!</span>
+		    <span>Amet assumenda dolore eos id iusto modi mollitia optio quaerat quam, quos recusandae sint? Ad aperiam aspernatur blanditiis commodi consequatur dolorum expedita illum ipsum itaque laborum pariatur praesentium, tempora tenetur?</span>
+	    </p>
     </div>
 </section>
 <?php get_footer(); ?>
