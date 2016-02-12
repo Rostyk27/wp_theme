5953c9d12bf48fe0be90954be29607523b4914f6
Rostyslav Chaikivskyi <r.chaikivskyi@gmail.com>
2016-02-12 09:40:26 +0200

    enough testing :)

--- 404.php
+++ 404.php
@@ -3,14 +3,6 @@
     <div class="container">
         <h1><span>404</span> error</h1>
         <p>Sorry, this page does not exist or has been removed.</p>
-	    hi!
-	    <h2>what?</h2>
-	    <p>
-	    	<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloremque dolorum, earum eos eum eveniet expedita illum incidunt ipsam ipsum labore maiores neque numquam porro saepe sit velit veniam voluptas!</span>
-	    </p>
-	    <h6>testtesttest</h6>
-	    <h4>for fun</h4>
-	    <h5>hello</h5>
     </div>
 </section>
 <?php get_footer(); ?>
