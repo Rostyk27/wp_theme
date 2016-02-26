b7e3c1e55d24c963b8afb1e39e4ff82fbd21b0ea
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2016-02-26 11:38:47 +0200

    fix jquery

--- inc/assets.php
+++ inc/assets.php
@@ -17,12 +17,13 @@ function tt_add_jscss() {
 		wp_enqueue_script('googlemaps', '//maps.googleapis.com/maps/api/js?v=3.exp', array(), '', false);
 	}
 
-	wp_enqueue_script('css3animateIt', get_stylesheet_directory_uri(). '/js/libs/css3animate-it.js', array('jquery'), '', true);
+	wp_enqueue_script('jquery', get_stylesheet_directory_uri(). '/js/libs/jquery.js', array(), '', false);
 	wp_enqueue_script('fastClick', get_stylesheet_directory_uri(). '/js/libs/fastclick.js', array('jquery'), '', true);
 	wp_enqueue_script('swiper', get_stylesheet_directory_uri(). '/js/libs/swiper.js', array('jquery'), '', true);
 	wp_enqueue_script('bx', get_stylesheet_directory_uri(). '/js/libs/bx.js', array('jquery'), '', true);
 	wp_enqueue_script('libs', get_stylesheet_directory_uri(). '/js/lib.js', array('jquery'), '', true);
 	wp_enqueue_script('logic', get_stylesheet_directory_uri(). '/js/logic.js', array('libs'), '', true);
+	wp_enqueue_script('css3animateIt', get_stylesheet_directory_uri(). '/js/libs/css3animate-it.js', array('jquery'), '', true);
 
 	wp_enqueue_style('animations', get_stylesheet_directory_uri(). '/style/animations.min.css' );
 	wp_enqueue_style('swiper', get_stylesheet_directory_uri(). '/style/swiper.css' );
