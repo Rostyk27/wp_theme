f89909414fa374f1fc99c774d44281ad69952153
Rostyslav Chaikivskyi <Rostyk27@users.noreply.github.com>
2016-03-23 10:42:28 +0200

    test

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <section class="content page404">
     <div class="container">
         <h1><span>404</span> error</h1>
-        <p>Sorry, this page does not exist or has been removed.</p>
+        <p>Sorry, this page doesn't exist or has been removed.</p>
     </div>
 </section>
 <?php get_footer(); ?>
