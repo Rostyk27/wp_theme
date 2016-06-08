d92e723b3009e362bcf6334e996edfee305fdb04
Ростик <r.chaikivskyi@gmail.com>
2016-06-08 11:06:19 +0300

    cleaning2

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 </div>
 <footer>
-    <div class="container">
+    <div class="container flex">
+        <?php if($copy = get_field('copy', 'option')) echo '<div class="copy">' . $copy . '</div>'; ?>
+        <?php if($dev = get_field('dev', 'option')) echo '<div class="dev">' . $dev . '</div>'; ?>
     </div>
 </footer>
 <?php wp_footer(); ?>
