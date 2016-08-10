0ae940a534ace836eee14368240aa460617780ad
Ростик <r.chaikivskyi@gmail.com>
2016-08-10 13:35:27 +0300

    remodal ex

--- footer.php
+++ footer.php
@@ -5,6 +5,14 @@
         <?php if($dev = get_field('dev', 'option')) echo '<div class="dev">' . $dev . '</div>'; ?>
     </div>
 </footer>
+<a href="#modal">Call modal!</a>
+<div class="remodal" data-remodal-id="modal">
+	<span data-remodal-action="close" class="remodal-close"></span>
+	<h2>Remodal</h2>
+	<h3>Example</h3>
+	<button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
+	<button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
