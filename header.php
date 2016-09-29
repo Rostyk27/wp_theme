d48876d377916d889226e971ffd77a9db75c62c4
Ростик <r.chaikivskyi@gmail.com>
2016-09-29 11:10:26 +0300

    favicon

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 <meta name="HandheldFriendly" content="True"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui, minimum-scale=1.0, maximum-scale=1.0"/>
 <meta name="apple-mobile-web-app-capable" content="yes"/>
-<link rel="shortcut icon" href="<?php echo theme('images/favicon.ico'); ?>" type="image/x-icon" sizes="16x16"/>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> data-hash="<?php wpa_fontbase64(true); ?>" data-a="<?php echo admin_url('admin-ajax.php'); ?>" >
