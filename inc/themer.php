ac015f3507d4c0219596fcbbdaa67064ed65e84f
Ростик <r.chaikivskyi@gmail.com>
2016-11-07 14:19:40 +0200

    dump

--- inc/themer.php
+++ inc/themer.php
@@ -475,3 +475,8 @@ function wpa_fontbase64($fonthash) {
 }
 add_action('wp_ajax_wpa_fontbase64', 'wpa_fontbase64');
 add_action('wp_ajax_nopriv_wpa_fontbase64', 'wpa_fontbase64');
+
+function wpa_dump($variable){
+	$pretty = function($v='',$c="&nbsp;&nbsp;&nbsp;&nbsp;",$in=-1,$k=null)use(&$pretty){$r='';if(in_array(gettype($v),array('object','array'))){$r.=($in!=-1?str_repeat($c,$in):'').(is_null($k)?'':"$k: ").'<br>';foreach($v as $sk=>$vl){$r.=$pretty($vl,$c,$in+1,$sk).'<br>';}}else{$r.=($in!=-1?str_repeat($c,$in):'').(is_null($k)?'':"$k: ").(is_null($v)?'&lt;NULL&gt;':"<strong>$v</strong>");}return$r;};
+	echo '<pre style="padding-left: 150px; font-family: Courier New"><code class="json">' . $pretty($variable) . '</code></pre>';
+}
