40526b56f3b06520f6d7ae7bce9b811ed0cc4f9e
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2019-06-15 17:47:36 +0300

    styles optimization

--- inc/assets.php
+++ inc/assets.php
@@ -26,3 +26,16 @@ function tt_add_jscss() {
 	wp_enqueue_style('style', get_stylesheet_directory_uri(). '/style/style.css' );
 }
 add_action('wp_enqueue_scripts', 'tt_add_jscss');
+
+// load styles in footer -> enable once loader added
+//function prefix_add_footer_styles() {
+//	wp_enqueue_style('libs', get_stylesheet_directory_uri(). '/style/libs.css' );
+//	wp_enqueue_style('style', get_stylesheet_directory_uri(). '/style/style.css' );
+//};
+//add_action( 'get_footer', 'prefix_add_footer_styles' );
+
+//Disable gutenberg style in Front
+function wps_deregister_styles() {
+	wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
