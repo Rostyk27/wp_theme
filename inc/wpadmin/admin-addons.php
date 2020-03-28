235d1a0abb67c3499d7598df30a8550a55e75b23
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2020-03-28 21:40:38 +0200

    upd deprecated functions

--- inc/wpadmin/admin-addons.php
+++ inc/wpadmin/admin-addons.php
@@ -138,9 +138,13 @@ add_action('wp_dashboard_setup', 'wpa_remove_dashboard_widgets');
 add_filter( 'show_admin_bar', '__return_false' ); // remove Admin Bar
 
 // Remove the wordpress update notifications for all users except Super Administator
-if (!current_user_can('update_plugins')) { // checks to see if current user can update plugins
-    add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-    add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+if( ! current_user_can('update_plugins')) { // checks to see if current user can update plugins
+	add_action('init', function() {
+		remove_action('init', 'wp_version_check');
+	}, 2);
+	add_filter('pre_option_update_core', function($a) {
+		return null;
+	});
 }
 
 // Changing the logo link from wordpress.org to root domain
