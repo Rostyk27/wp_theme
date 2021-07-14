3be6fb90b4ffb135b13d326af65963a455ad2523
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2021-07-14 14:53:44 +0300

    a11y - cf7 inputs focus upd

--- js/scripts.js
+++ js/scripts.js
@@ -67,6 +67,12 @@ $(document).ready(function() {
         });
         $(this).parents('.wpcf7-form').find('.wpcf7-response-output').addClass('is_temp_hidden');
     });
+    $(this).on('focus', '.wpcf7-form-control.wpcf7-not-valid', function() {
+        $(this).next('.wpcf7-not-valid-tip').fadeOut(250,function(){
+            $(this).remove();
+        });
+        $(this).parents('.wpcf7-form').find('.wpcf7-response-output').addClass('is_temp_hidden');
+    });
     document.addEventListener( 'wpcf7submit', function( event ) {
         $('.wpcf7-response-output').removeClass('is_temp_hidden');
     }, false );
