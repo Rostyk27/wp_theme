272f5ce2b319ed1ed55c7f5a4bd6b06f5415ffbf
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2021-12-23 16:39:32 +0200

    сÐcpt - functions for taxonomy args

--- inc/cpt.php
+++ inc/cpt.php
@@ -1,32 +1,36 @@
 <?php
-//Example for Custom Post Type with Taxonomies
+// example for Custom Post Type with Taxonomies
 
-/*
-    *** You van use dash-icons https://developer.wordpress.org/resource/dashicons/
-*/
-add_action( 'init', 'register_cpts' );
-function register_cpts() {
+// you can use dash-icons https://developer.wordpress.org/resource/dashicons/
 
-    //custom taxonomy attached to CPT
-	$taxname   = 'Taxonomy Name';
-	$taxlabels = array(
-		'name'                       => $taxname,
-		'singular_name'              => $taxname,
-		'search_items'               => 'Search ' . $taxname,
-		'popular_items'              => 'Popular ' . $taxname,
-		'all_items'                  => 'All ' . $taxname . 's',
-		'parent_item'                => 'Parent ' . $taxname,
-		'edit_item'                  => 'Edit ' . $taxname,
-		'update_item'                => 'Update ' . $taxname,
-		'add_new_item'               => 'Add New ' . $taxname,
-		'new_item_name'              => 'New ' . $taxname,
-		'separate_items_with_commas' => 'Separate ' . $taxname . 's with commas',
-		'add_or_remove_items'        => 'Add or remove ' . $taxname . 's',
-		'choose_from_most_used'      => 'Choose from most used ' . $taxname . 's'
+
+/**
+ * custom taxonomy args
+ *
+ * @param $tax_name
+ *
+ * @return array
+ */
+function tax_args_arr($tax_name) {
+	$tax_labels = array(
+		'name'                       => $tax_name,
+		'singular_name'              => $tax_name,
+		'search_items'               => 'Search ' . $tax_name,
+		'popular_items'              => 'Popular ' . $tax_name,
+		'all_items'                  => 'All ' . $tax_name . 's',
+		'parent_item'                => 'Parent ' . $tax_name,
+		'edit_item'                  => 'Edit ' . $tax_name,
+		'update_item'                => 'Update ' . $tax_name,
+		'add_new_item'               => 'Add New ' . $tax_name,
+		'new_item_name'              => 'New ' . $tax_name,
+		'separate_items_with_commas' => 'Separate ' . $tax_name . 's with commas',
+		'add_or_remove_items'        => 'Add or remove ' . $tax_name . 's',
+		'choose_from_most_used'      => 'Choose from most used ' . $tax_name . 's'
 	);
-	$taxarr    = array(
-		'label'             => $taxname,
-		'labels'            => $taxlabels,
+
+	return array(
+		'label'             => $tax_name,
+		'labels'            => $tax_labels,
 		'public'            => true,
 		'hierarchical'      => true,
 		'show_in_nav_menus' => true,
@@ -37,8 +41,60 @@ function register_cpts() {
 		'show_admin_column' => true,
 		'show_in_rest'      => true,
 	);
-    register_taxonomy( 'custom_taxonomy_name', 'custom_post_type', $taxarr );
+}
 
+
+/**
+ * custom taxonomy args - when plural is custom
+ *
+ * @param $tax_name
+ * @param $tax_name_plural
+ *
+ * @return array
+ */
+function tax_args_arr_plural($tax_name, $tax_name_plural) {
+	$tax_labels = array(
+		'name'                       => $tax_name,
+		'singular_name'              => $tax_name,
+		'search_items'               => 'Search ' . $tax_name,
+		'popular_items'              => 'Popular ' . $tax_name,
+		'all_items'                  => 'All ' . $tax_name_plural,
+		'parent_item'                => 'Parent ' . $tax_name,
+		'edit_item'                  => 'Edit ' . $tax_name,
+		'update_item'                => 'Update ' . $tax_name,
+		'add_new_item'               => 'Add New ' . $tax_name,
+		'new_item_name'              => 'New ' . $tax_name,
+		'separate_items_with_commas' => 'Separate ' . $tax_name_plural . ' with commas',
+		'add_or_remove_items'        => 'Add or remove ' . $tax_name_plural,
+		'choose_from_most_used'      => 'Choose from most used ' . $tax_name_plural
+	);
+
+	return array(
+		'label'             => $tax_name,
+		'labels'            => $tax_labels,
+		'public'            => true,
+		'hierarchical'      => true,
+		'show_in_nav_menus' => true,
+		'args'              => array( 'orderby' => 'term_order' ),
+		'query_var'         => true,
+		'show_ui'           => true,
+		'rewrite'           => true,
+		'show_admin_column' => true,
+		'show_in_rest'      => true,
+	);
+}
+
+
+add_action( 'init', 'register_cpts' );
+
+function register_cpts() {
+
+	// args for taxonomy "custom_taxonomy_name"
+	$tax_arr  = tax_args_arr( 'Taxonomy Name' );
+	// taxonomy "custom_taxonomy_name" attached to cpt "custom_post_type"
+	register_taxonomy( 'custom_taxonomy_name', 'custom_post_type', $tax_arr );
+
+	// cpt "custom_post_type"
 	register_post_type( 'custom_post_type',
 		array(
 			'labels'            => array(
@@ -49,7 +105,7 @@ function register_cpts() {
 			'public'            => true,
 			'show_ui'           => true,
 			'show_in_menu'      => true,
-			'supports'          => array( 'title', 'editor', 'thumbnail' ),
+			'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
 			'rewrite'           => array( 'slug' => 'permalink' ),
 			'has_archive'       => true,
 			'hierarchical'      => true,
@@ -59,6 +115,7 @@ function register_cpts() {
 			'show_in_rest'      => true,
 			'menu_icon'         => 'dashicons-admin-page',
 		) );
+
     if( defined('WP_DEBUG') && true !== WP_DEBUG) {
         flush_rewrite_rules();
     }
