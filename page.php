1788076c6bee57d0a9d41296627f7f6f06554701
Volodymyr Senyk <vova.senyk@gmail.com>
2022-03-22 22:00:24 +0200

    upd page top-panel

--- page.php
+++ page.php
@@ -1,15 +1,22 @@
 <?php get_header(); ?>
 
-<section class="top_panel top_panel__secondary" style="<?php echo has_post_thumbnail() ? esc_attr(image_src( get_post_thumbnail_id( $post->ID ), 'full', true )) : 'background-image: url('. esc_url(theme('images/top_default.jpg')) .')'; ?>">
-    <div class="container">
-        <h1><?php the_title(); ?></h1>
-    </div>
+<section class="top_panel top_panel__secondary">
+    <picture>
+        <source media="(max-width: 480px)"
+                srcset="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id( get_the_ID() ), 'mob_size' ); ?>">
+		<?php echo wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ), 'full', false, array(
+			'alt'   => get_alt( get_the_ID() ),
+			'class' => 'object_fit'
+		) ); ?>
+    </picture>
 </section>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); if ( get_the_content() ) : ?>
-    <section class="default_page content container">
-        <?php the_content(); ?>
-    </section>
-<?php endif; endwhile; endif; ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+	if ( get_the_content() ) : ?>
+        <section class="default_page content container">
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        </section>
+	<?php endif; endwhile; endif; ?>
 
 <?php get_footer(); ?>
--- tpl-contact.php
+++ tpl-contact.php
@@ -3,8 +3,15 @@
 wp_enqueue_script( 'selectric', get_stylesheet_directory_uri() . '/js/libs/selectric.js', array( 'jquery' ), null, true );
 ?>
 
-<section class="top_panel top_panel__secondary"
-         style="<?php echo has_post_thumbnail() ? esc_attr( image_src( get_post_thumbnail_id( $post->ID ), 'full', true ) ) : 'background-image: url(' . esc_url( theme( 'images/top_default.jpg' ) ) . ')'; ?>">
+<section class="top_panel top_panel__secondary">
+    <picture>
+        <source media="(max-width: 480px)"
+                srcset="<?php echo wp_get_attachment_image_url( get_post_thumbnail_id( get_the_ID() ), 'mob_size' ); ?>">
+		<?php echo wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ), 'full', false, array(
+			'alt'   => get_alt( get_the_ID() ),
+			'class' => 'object_fit'
+		) ); ?>
+    </picture>
     <div class="container">
         <h1><?php the_title(); ?></h1>
     </div>
@@ -14,7 +21,8 @@ wp_enqueue_script( 'selectric', get_stylesheet_directory_uri() . '/js/libs/selec
 <section class="default_page container">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 		if ( get_the_content() ) : ?>
-			<div class="content"><?php the_content(); ?></div>
+			<div class="content">
+                <?php the_content(); ?></div>
 		<?php endif; endwhile; endif; ?>
     <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]'); ?>
 </section>
