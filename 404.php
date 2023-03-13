8fe30d0ce930d6f29b369716adfe4bff5e22b197
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-03-13 14:08:49 +0200

    test

--- 404.php
+++ 404.php
@@ -8,6 +8,7 @@ tpl_style( 'page' );
 
 <div class="page_404 default_page container">
     <div class="content">
+        <h2>test</h2>
         <p>Sorry, this page doesn't exist or has been removed.</p>
         <a href="<?php echo esc_url( site_url() ); ?>" class="button">Go back home</a>
     </div>
